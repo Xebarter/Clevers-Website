cb331ca29656a1eddaae454447c458854b1b8459
Xebarter <sebenock027@gmail.com>
2025-04-17 10:26:35 +0300

    added sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+    siteUrl: 'https://cleversoriginschools.com', // change to your live domain
+    generateRobotsTxt: true,
+    sitemapSize: 5000,
+  };
+  
